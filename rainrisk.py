cb3ce56d60df70d53dc41cd40620b947bd9f7e0b
Graham Ashton <graham@effectif.com>
2021-08-18 10:32:15 +0100

    Introduce Step to separate x and y axes
    We need to be able to sum up the distance travelled in the N/S and E/W
    axes independently.

--- rainrisk.py
+++ rainrisk.py
@@ -3,14 +3,27 @@ class Journey:
         self.steps = []
 
     def travel(self, instruction):
-        self.steps.append(self._parse_instruction(instruction))
+        self.steps.append(Step(instruction[0], int(instruction[1:])))
 
     @property
     def manhattan_distance(self):
-        return sum([step[1] for step in self.steps])
+        horizontal = sum([step.horizontal for step in self.steps])
+        vertical = sum([step.vertical for step in self.steps])
+        return abs(horizontal) + abs(vertical)
 
-    def _parse_instruction(self, instruction):
-        direction = instruction[0]
-        distance = int(instruction[1:])
-        signs = {"N": 1, "S": -1}
-        return (direction, distance * signs[direction])
+
+class Step:
+    def __init__(self, direction, distance):
+        self.direction = direction
+        self.distance = distance
+
+    @property
+    def horizontal(self):
+        return self._distance_along_axis({"N": 0, "S": 0, "E": 1, "W": -1})
+
+    @property
+    def vertical(self):
+        return self._distance_along_axis({"N": 1, "S": -1, "E": 0, "W": 0})
+
+    def _distance_along_axis(self, signs):
+        return self.distance * signs[self.direction]
--- test_rainrisk.py
+++ test_rainrisk.py
@@ -15,6 +15,29 @@ class JourneyTest(unittest.TestCase):
         journey.travel("S1")
         self.assertEqual(0, journey.manhattan_distance)
 
+    def test_moving_east_sets_manhattan_distance(self):
+        journey = rainrisk.Journey()
+        journey.travel("E1")
+        self.assertEqual(1, journey.manhattan_distance)
+
+    def test_moving_east_then_west_sets_correct_manhattan_distance(self):
+        journey = rainrisk.Journey()
+        journey.travel("E1")
+        journey.travel("W1")
+        self.assertEqual(0, journey.manhattan_distance)
+
+    def test_moving_west_sets_manhattan_distance(self):
+        journey = rainrisk.Journey()
+        journey.travel("W1")
+        self.assertEqual(1, journey.manhattan_distance)
+
+    def test_moving_on_both_axes_sets_manhattan_distance(self):
+        journey = rainrisk.Journey()
+        journey.travel("W1")
+        journey.travel("S3")
+        journey.travel("E2")
+        self.assertEqual(4, journey.manhattan_distance)
+
 
 if __name__ == "__main__":
     unittest.main()
